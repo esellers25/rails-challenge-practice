b51897ad6c3e4cc0578f28d5c636c1f165796079
esellers25 <erin.e.sellers@gmail.com>
2021-05-24 21:43:18 -0400

    working on company show page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,25 @@
 class CompaniesController < ApplicationController
+
+    def show 
+        @company = Company.find(params[:id])
+        @employee = Employee.new 
+    end 
+
+    def new 
+        @company = Company.new 
+    end 
+
+    def create
+        @company = Company.new(company_params)
+    end 
+
+    private 
+    def company_params
+        params.require(:company).permit(:name)
+    end 
+
+    def employee_params
+        params.require(:employee).permit(:name, :title)
+    end 
+
 end
